97b9ea1dd6feb6d12a782c72f0d892c11b13fd7e
Imma7 <immamugambi@gmail.com>
2018-07-24 12:02:06 +0300

    Created Image model

---
+++ instagram/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length = 30)
+    image_caption = models.CharField(max_length = 30, blank = True)
+    comments = models.TextField(max_length=50, blank=True)
+    likes = models.IntegerField()
+    
