09f85763872943a87c7595049155c087ed64b573
Imma7 <immamugambi@gmail.com>
2018-07-24 13:01:40 +0300

    Updated models

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    bio = models.TextField(max_length=50, blank=True)
+    username = models.CharField(max_length=30)
+    user = models.ForeignKey(User, blank=True)
+
+    def save_profile(self):
+        return self.save()
+
+    def update_profile(self):
+        return self.update()
+
+    def delete_profile(self):
+        return self.delete()
+
+    @classmethod
+    def find_profile(search_profile, cls):
+        profile = cls.objects.filter(username = search_profile)
+        return profile
+
+
 class Image(models.Model):
     image = models.ImageField()
     image_name = models.CharField(max_length = 30)
@@ -24,22 +46,3 @@ class Image(models.Model):
         return image
 
 
-class Profile(models.Model):
-    profile_photo = models.ImageField()
-    bio = models.TextField(max_length=50, blank=True)
-    username = models.CharField()
-    user = models.ForeignKey(User, blank=True)
-
-    def save_profile(self):
-        return self.save()
-
-    def update_profile(self):
-        return self.update()
-
-    def delete_profile(self):
-        return self.delete()
-
-    @classmethod
-    def find_profile(search_profile, cls):
-        profile = cls.objects.filter(username = search_profile)
-        return profile
