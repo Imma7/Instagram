0b1356a9a0c9b7c9516660ec3c9bfcbd6e033375
Imma7 <immamugambi@gmail.com>
2018-07-25 14:56:34 +0300

    Created views function

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import Image, Profile
+import datetime as dt
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return HttpResponse('Instagram clone')
+    date = dt.date.today()
+    image = Image.get_all()
+
+    return render(request, 'index.html', {"image":image})
