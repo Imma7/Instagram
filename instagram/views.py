b4803bc3b24e062f169dd71d3e56992fc04dd0e0
Imma7 <immamugambi@gmail.com>
2018-07-26 12:11:41 +0300

    Created new post view function

--- instagram/views.py
+++ instagram/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Image, Profile
 import datetime as dt
 from django.contrib.auth import logout
+from .forms import NewImageForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -16,3 +17,16 @@ def index(request):
 def logout(request):
     logout(request)
     return render(request, 'login.html')
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit = False)
+            image.profile = current_user
+            image.save()
+        else:
+            form = NewImageForm()
+        return render(request, 'new_post.html', {"form": form})
