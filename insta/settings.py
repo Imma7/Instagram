ac5a09fc2c5ec969a0aa21936fab5bab53485654
Imma7 <immamugambi@gmail.com>
2018-07-26 11:00:11 +0300

    Configure email server settings

--- insta/settings.py
+++ insta/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
-    'bootstrap',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '1018.laflare@gmail.com'
+EMAIL_HOST_PASSWORD = 'laflare1020'
+EMAIL_PORT = 587
