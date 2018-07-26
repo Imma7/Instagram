044d2db87587359977ffcfb52cb803822525782b
Imma7 <immamugambi@gmail.com>
2018-07-26 11:45:10 +0300

    Created logout view function

--- instagram/views.py
+++ instagram/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Image, Profile
 import datetime as dt
+from django.contrib.auth import logout
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -11,3 +12,7 @@ def index(request):
     image = Image.get_all()
 
     return render(request, 'index.html', {"image":image})
+
+def logout(request):
+    logout(request)
+    return render(request, 'login.html')
