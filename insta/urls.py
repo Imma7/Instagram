da741b30002fab135b6520d41774f936d9c9eb24
Imma7 <immamugambi@gmail.com>
2018-07-26 11:54:22 +0300

    Created tinymce url patterns

--- insta/urls.py
+++ insta/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'', include('instagram.urls'))
+    url(r'', include('instagram.urls')),
     url(r'^logout/$', views.logout, {"next_page":'/'}),
+    url(r'^tinymce/', include('tinymce.urls')),
 ]
