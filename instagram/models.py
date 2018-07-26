2b6d6200538b0f22ed149e7bf09f8bf2c71527af
Imma7 <immamugambi@gmail.com>
2018-07-26 11:53:49 +0300

    Updated comment field to TextField

--- instagram/models.py
+++ instagram/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt 
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profile/')
-    bio = models.TextField(max_length=50, blank=True)
+    bio = models.TextField(max_length=50, blank=True, null=True)
     username = models.CharField(max_length=30)
-    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, null=True)
 
     def save_profile(self):
         return self.save()
@@ -22,15 +24,17 @@ class Profile(models.Model):
         profile = cls.objects.filter(username = search_profile)
         return profile
 
+    def __str__(self):
+        return self.username
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to = 'gallery/')
     image_name = models.CharField(max_length = 30)
-    image_caption = models.CharField(max_length = 30, blank = True)
-    comments = models.TextField(max_length=50, blank=True)
+    image_caption = models.CharField(max_length = 30, blank = True, null=True)
+    comments = HTMLField()
     likes = models.IntegerField()
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def save_image(self):
         return self.save()
@@ -46,4 +50,10 @@ class Image(models.Model):
         image = cls.objects.filter(image_name = search_image)
         return image
 
+    @classmethod
+    def get_all(cls):
+       images = cls.objects.order_by('-pub_date')
+       return images
 
+    def __str__(self):
+        return self.image_name
