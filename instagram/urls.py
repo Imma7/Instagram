7ce969438f642b4c1b61c73e8521a47dc7a533c2
Imma7 <immamugambi@gmail.com>
2018-07-26 12:14:17 +0300

    Created new post url pattern

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
 
 urlpatterns = [
-    
+    url('^$', views.index, name = 'index'),
+    url(r'^new/post$', views.new_post, name='new-post')
 ]
 
 if settings.DEBUG:
