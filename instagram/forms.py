01a76e55d0eb5b8691836c53cc9cb7e2db05e8a4
Imma7 <immamugambi@gmail.com>
2018-07-30 04:08:55 +0300

    Updated forms

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,7 +1,8 @@
 from .models import Image
+from django import forms 
 
 class NewImageForm(forms.ModelForm):
     class Meta:
          model = Image
          exclude = ['profile', 'pub_date']
-         
+
