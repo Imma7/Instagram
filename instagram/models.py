5f12d4ca3bc8d91c1819beabbcabbacdb869b559
Imma7 <immamugambi@gmail.com>
2018-07-30 04:09:19 +0300

    Updated model classes

--- instagram/models.py
+++ instagram/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt 
 from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+      
 # Create your models here.
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to = 'profile/')
+    profile_photo = models.ImageField(upload_to = 'profile/', blank=True)
     bio = models.TextField(max_length=50, blank=True, null=True)
-    username = models.CharField(max_length=30)
-    user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, null=True)
+    # username = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE ,related_name='profile') 
 
     def save_profile(self):
         return self.save()
@@ -24,16 +35,23 @@ class Profile(models.Model):
         profile = cls.objects.filter(username = search_profile)
         return profile
 
+    @classmethod
+    def get_by_id(cls,id):
+        profile=Profile.objects.filter(user=id)
+        return profile
+
+    
+
     def __str__(self):
-        return self.username
+        return self.user.username
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'gallery/')
     image_name = models.CharField(max_length = 30)
     image_caption = models.CharField(max_length = 30, blank = True, null=True)
-    comments = HTMLField()
+    comments = models.TextField()
     likes = models.IntegerField()
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posted_by", null = True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def save_image(self):
@@ -55,5 +73,15 @@ class Image(models.Model):
        images = cls.objects.order_by('-pub_date')
        return images
 
+    @classmethod
+    def get_profile_images(cls,profile):
+        images = Image.objects.filter(profile_pk=profile)
+        return images
+
     def __str__(self):
         return self.image_name
+
+class Likes(models.Model):
+    postid = models.IntegerField()
+    liker = models.CharField(max_length = 20)
+    
