09d09661151e9a73c6a157e100c3010ef4f46451
Imma7 <immamugambi@gmail.com>
2018-07-30 04:08:36 +0300

    Configure url patterns

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,7 +7,8 @@ from . import views
 urlpatterns = [
     url('^$', views.index, name = 'index'),
     url(r'^new/post$', views.new_post, name='new-post'),
-    url(r'^my-profile/(?P<profile_id>\d+)' , views.profile, name = 'profile')
+    url(r'^my-profile/(?P<user_id>\d+)' , views.profile, name = 'profile'),
+    url(r'images_profile/(?P<id>\d+)', views.images_profile, name='images_for_profile')
 ]
 
 if settings.DEBUG:
