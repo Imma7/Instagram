1cab524436846baa2816dc268b85469e1724d0f7
Imma7 <immamugambi@gmail.com>
2018-07-30 09:28:02 +0300

    Added user creation form

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,8 +1,15 @@
 from .models import Image
-from django import forms 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
 
 class NewImageForm(forms.ModelForm):
     class Meta:
          model = Image
          exclude = ['profile', 'pub_date']
 
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
