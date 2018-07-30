afb787a94942222e7d42c92d8122a5dc2fe51c1b
Imma7 <immamugambi@gmail.com>
2018-07-30 11:15:16 +0300

    Installed whitenoise

--- insta/settings.py
+++ insta/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'insta.urls'
