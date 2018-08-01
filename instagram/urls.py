1cb922ac0ff4d9f02463d3acb857f22821895cb2
Imma7 <immamugambi@gmail.com>
2018-08-01 07:16:15 +0300

    Added search urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    url(r'^search/', views.search_profile, name='search_profile'),
 ]
 
 if settings.DEBUG:
